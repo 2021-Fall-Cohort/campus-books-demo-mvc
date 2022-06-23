8a01bc4930a214541eddd438bddf75d64c6d64d5
davidhooper-WCCI <david@wecancodeit.org>
2022-06-23 11:11:45 -0400

    end of forms

--- src/main/java/wcci/demo/controllers/BookController.java
+++ src/main/java/wcci/demo/controllers/BookController.java
@@ -3,18 +3,27 @@ package wcci.demo.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import wcci.demo.model.Author;
+import wcci.demo.model.Book;
+import wcci.demo.repos.AuthorRepository;
 import wcci.demo.repos.BookRepository;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/books")
 public class BookController {
 
     private BookRepository bookRepo;
+    private AuthorRepository authorRepo;
 
 
-    public BookController(BookRepository bookRepo) {
+    public BookController(BookRepository bookRepo, AuthorRepository authorRepo) {
         this.bookRepo = bookRepo;
+        this.authorRepo = authorRepo;
     }
 
     @RequestMapping("/{id}")
@@ -28,5 +37,24 @@ public class BookController {
         model.addAttribute("books",bookRepo.findAll());
         return "allBooks";
     }
+    @PostMapping("/{id}/addAuthor")
+    public String addAuthorToBook(@PathVariable Long id, @RequestParam String author){
+        Book book = bookRepo.findById(id).get();
+        Optional<Author> authorOptional = authorRepo.findByNameIgnoreCase(author);
+        if(authorOptional.isPresent()){
+            if(!book.getAuthors().contains(authorOptional.get())){
+                book.addAuthor(authorOptional.get());
+            }
+        }
+        else{
+            Author author1 = new Author(author);
+            authorRepo.save(author1);
+            book.addAuthor(author1);
+        }
+        bookRepo.save(book);
 
+        return "redirect:/books/"+id;
+
+
+    }
 }
--- src/main/java/wcci/demo/repos/AuthorRepository.java
+++ src/main/java/wcci/demo/repos/AuthorRepository.java
@@ -3,5 +3,9 @@ package wcci.demo.repos;
 import org.springframework.data.repository.CrudRepository;
 import wcci.demo.model.Author;
 
+import java.util.Optional;
+
 public interface AuthorRepository extends CrudRepository<Author, Long> {
+    Optional<Author> findByNameIgnoreCase(String name);
+
 }
